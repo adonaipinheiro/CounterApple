4c7fd9193373115f74c8aa6ebba0ba680d2084e1
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2025-03-22 17:08:02 -0300

    Initial Commit

---
+++ CounterApple/CounterAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppleApp.swift
+//  CounterApple
+//
+//  Created by Adonai Pinheiro on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
